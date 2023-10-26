0289ae27d218df1d7397a048e072d8c71c35300e
kamleshSQT <kamlesh.prajapati@softqubes.com>
2023-10-26 15:57:18 +0530

    change base path again

--- src/CommonServices/index.js
+++ src/CommonServices/index.js
@@ -1,6 +1,5 @@
 const API_BASE_PATH = {
-  // BasePath: "http://localhost:3000/api/",
-  BasePath: "https://nodeapisproject.vercel.app/",
+  BasePath: "http://localhost:3000/api/",
 };
 export default API_BASE_PATH;
 
